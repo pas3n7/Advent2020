61cf135f5c8b3dd43734447c137f83216446e35c
Patrick Smith <pas3n7@gmail.com>
2020-12-04 18:34:03 -0600

    Day 3 part 1 completed

---
+++ Day3/Day3.1.ps1
@@ -0,0 +1,26 @@
+$map = Get-Content -Path ".\Day3\input.txt"
+$numtrees = 0
+$sloperight = 3 #(x)
+$slopedown = 1 #(y)
+$position = @(0,0) #(x,y)
+$tree = '#'
+$numlines = $map.Count
+$width = $map[0].length
+
+
+##remember that map[y][x]
+
+while (($position[1] + $slopedown) -le $numlines  )
+{
+
+	#check if we are on a tree
+	if ($map[$position[1]][$position[0]] -eq $tree)
+	{
+		$numtrees++
+	}
+
+	#now move
+	$position[0] = ($position[0] + $sloperight) % $width
+	$position[1] += $slopedown
+}
+Write-Output $numtrees
