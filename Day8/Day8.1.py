006d6cf857ccc10c91990544a34298fb3b0f0837
Patrick Smith <pas3n7@gmail.com>
2020-12-09 18:21:05 -0600

    Running, but wrong

--- Day8/Day8.1.py
+++ Day8/Day8.1.py
@@ -11,10 +11,28 @@ class operation:
 		if (type == "jmp"):
 			self.pointsto = position + value
 		else:
-			self.pointsto = 1
+			self.pointsto = self.position + 1
 		if (self.pointsto < 0):
 			self.pointsto = None
+		self.visited = False
 
 with open(r'.\Day8\input.txt') as thefile:
 	for index, line in enumerate(thefile):
-		thecode.append(operation(index, line[0,3], int(line[4,8])))
+		thecode.append(operation(index, line[0:3], int(line[4:8])))
+
+# for i in thecode:
+# 	print(str(i.position) + " " + i.type + " " + str(i.value) + " points to:" + str(i.pointsto))
+
+currentop = 0
+accum = 0
+ReachedLoop = False
+
+while ReachedLoop is False:
+	ReachedLoop = thecode[currentop].visited
+	thecode[currentop].visited = True
+	if (thecode[currentop].type == "acc"):
+		accum += thecode[currentop].value
+
+	currentop = thecode[currentop].pointsto
+
+print(accum)
