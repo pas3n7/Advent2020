c95fd2dbebcc0c2dffdfe26735665c76c339afd7
Patrick Smith <pas3n7@gmail.com>
2020-12-17 17:16:30 -0600

    Fixed the issue with expansion going nuts

--- Day17/Day17.1.py
+++ Day17/Day17.1.py
@@ -1,4 +1,4 @@
-FILENAME = r".\Day17\input.txt"
+FILENAME = r".\Day17\testinput.txt"
 from copy import deepcopy
 
 class threedgol:
@@ -14,27 +14,23 @@ class threedgol:
 		tempspaceslice = inputdata.split("\n")
 		self.dimension = len(tempspaceslice)
 		self.space[0] = [list(row) for row in tempspaceslice] #list(row) turns the string into a list of chars
-		self.expand2() 		#expand by 2, so we don't have to worry about checking indexes later
+		self.expand(2) 		#expand by 2, so we don't have to worry about checking indexes later
 
-	def expand1(self):
-		self.space[0].insert(0, ['.']*(self.dimension))
-		self.space[0].append(['.']*(self.dimension))
-		for i in self.space[0]:
-			i.insert(0, '.')
-			i.append('.')
-		self.dimension += 2
-		self._expandz()
-
-	def expand2(self):
-		self.expand1()
-		self.expand1()
-	
-	def _expandz(self):
-		blankspace = [['.']*self.dimension]*self.dimension
-		self.space.insert(0, blankspace)
-		self.space.append(blankspace)
-		self.centersliceref += 1
-		self.dimensionz += 2
+	def expand(self, num):
+		#add all-off z slices to top and bottom
+		for num in range(num):
+			dimension = self.dimension
+			self.space.insert(0, [[ '.' for i in range(dimension) ] for j in range(dimension)])
+			self.space.append([[ '.' for i in range(dimension) ] for j in range(dimension)])
+			self.centersliceref += 1
+			self.dimensionz += 2
+			for zslice in self.space:
+				zslice.insert(0, ['.' for i in range(dimension)])
+				zslice.append(['.' for i in range(dimension)])
+				for i in zslice:
+					i.insert(0, '.')
+					i.append('.')
+			self.dimension += 2
 
 	def countactivenei(self, dimension):
 		#pass in dimension as a tuple (z, y, x)
@@ -52,31 +48,32 @@ class threedgol:
 		active = [y for x in active for y in x].count("#")
 		return active
 
-	def rungeneration(self):
-		tempspace = deepcopy(self.space)
-		activesym = '#'
-		inactivesym = '.'
-		markedforexpand = False
-		#if active and 2 or 3 neighbors are active, keep active, otherwise make inactive
-		#if inactive but 3 neighbors active, stays inactive, otherwise make active
-		#outside is always empty, don't need to check,
-		for z in range(1, self.dimensionz-1):
-			for y in range(1, self.dimension-1):
-				for x in range(1, self.dimension-1):
-					madechange = False
-					if self.space[z][y][x] == activesym and self.countactivenei((z, y, x)) not in [2, 3]:
-						tempspace[z][y][x] = inactivesym
-						madechange = True
-					elif self.space[z][y][x] == inactivesym and self.countactivenei((z, y, x)) not in [3]:
-						tempspace[z][y][x] = activesym
-						madechange = True
+	def rungeneration(self, gens= 1):
+		for gen in range(gens):
+			tempspace = deepcopy(self.space)
+			activesym = '#'
+			inactivesym = '.'
+			markedforexpand = False
+			#if active and 2 or 3 neighbors are active, keep active, otherwise make inactive
+			#if inactive but 3 neighbors active, stays inactive, otherwise make active
+			#outside is always empty, don't need to check,
+			for z in range(1, self.dimensionz-1):
+				for y in range(1, self.dimension-1):
+					for x in range(1, self.dimension-1):
+						madechange = False
+						if self.space[z][y][x] == activesym and self.countactivenei((z, y, x)) not in [2, 3]:
+							tempspace[z][y][x] = inactivesym
+							madechange = True
+						elif self.space[z][y][x] == inactivesym and self.countactivenei((z, y, x)) not in [3]:
+							tempspace[z][y][x] = activesym
+							madechange = True
 
-					if madechange:
-						if x in [1, self.dimension - 2] or y in [1, self.dimension - 2] or z in [self.centersliceref -1, self.centersliceref + 1]:
-							markedforexpand = True
-		self.space[0] = tempspace
-		if markedforexpand:
-			self.expand1()
+						if madechange:
+							if x in [1, self.dimension - 2] or y in [1, self.dimension - 2] or z in [self.centersliceref -1, self.centersliceref + 1]:
+								markedforexpand = True
+			self.space = tempspace
+			if markedforexpand:
+				self.expand(1)
 	def getcenterslice(self):
 		return self.space[self.centersliceref]
 
@@ -89,15 +86,25 @@ with open(FILENAME) as thefile:
 
 myspace = threedgol(rawdata)
 
-for x in myspace.getcenterslice():
-	print(x)
+myspace.rungeneration(1)
+for z in range(myspace.dimensionz):
+	for x in myspace.space[z]:
+		print(x)
+	print('\n')
 print(myspace.countactive())
 
-for i in range(6):
-	myspace.rungeneration()
-	print("\n")
-	for x in myspace.getcenterslice():
-		print(x)
-	print(myspace.countactive())
 
-print(myspace.countactivenei((7, 3))  in [2, 3])
+
+
+# for x in myspace.getcenterslice():
+# 	print(x)
+
+# print("\n")
+
+
+# for i in range(2):
+# 	myspace.rungeneration()
+# 	print("\n")
+# 	for x in myspace.getcenterslice():
+# 		print(x)
+# 	print(myspace.countactive())
