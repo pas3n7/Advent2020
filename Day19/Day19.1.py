9d3a60314c2f3315cde9fd8f8c65a8b6d012b722
Patrick Smith <pas3n7@gmail.com>
2020-12-23 17:29:30 -0600

    Added reverse reference

--- Day19/Day19.1.py
+++ Day19/Day19.1.py
@@ -8,12 +8,19 @@ with open(FILENAME) as thefile:
 
 class ruleset:
 	def __init__(self, rulesetraw):
+
+		self.rulesetraw = rulesetraw.split('\n')
+		self.numrules = len(self.rulesetraw)
+		self.revref = [set() for _ in range(self.numrules)] ##we will be able to look up all references to a particular index in ruleset. will init in readindata
+		
 		#init ruleset
 		self.ruleset = []
-		self._readindata(rulesetraw)
+		self._readindata(self.rulesetraw)
+		
+
 	
 	def _readindata(self, rulesetraw):
-		rulesetraw = rulesetraw.split('\n')
+
 		refregp = r'(\d): ([\d ]+)(?:\|([\d ]+))?'
 		letregp = r'(\d): "([ab])"'
 		for line in rulesetraw:
@@ -21,8 +28,12 @@ class ruleset:
 			if match:
 				matchnum = int(match.group(1))
 				matchset1 = [int(x) for x in match.group(2).strip().split(' ')]
+				for n in matchset1:
+					self.revref[n].add(matchnum) #add references to matchnum
 				try:
 					matchset2 = [int(x) for x in match.group(3).strip().split(' ')]
+					for n in matchset2:
+						self.revref[n].add(matchnum) #add references to matchnum
 				except:
 					matchset2 = None
 				self.ruleset.append([matchnum, matchset1, matchset2])
@@ -41,3 +52,6 @@ myruleset = ruleset(rawdata[0])
 
 for rule in myruleset.ruleset:
 	print(rule)
+
+print(myruleset.revref)
+print(myruleset.numrules)
