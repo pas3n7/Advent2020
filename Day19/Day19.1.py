8097e5f6468eecc57c465e7a4e1414c612db2044
Patrick Smith <pas3n7@gmail.com>
2020-12-25 19:16:47 -0600

    Completed day 19 part 1

--- Day19/Day19.1.py
+++ Day19/Day19.1.py
@@ -1,6 +1,16 @@
+## in retrospect, this is a nonsensical solution. My best idea instead is to instead of focusing on a ruleset class, make a rule class, then generate a list 
+# of references to that rule to be contained in the rule itself. Then just follow the references around recursively. 
+# probably some much better way to do it, but that's the best I can think of while I sit here regretting this approach
+## Perhaps part 2 will require a complete rewite anyway, who knows
+#
+# What lead me to this solution was that on previous days, I was taking too long trying to think of the 'best' way to solve the problem before I got 
+# started coding, so for this one I decided to just come up with ANY idea and get stuck in. 
+#
+## This is not a failure, however. The lessons learned here were worth the cost. 
+
 import re
 
-FILENAME = r'.\Day19\testinput.txt'
+FILENAME = r'.\Day19\input.txt'
 
 with open(FILENAME) as thefile:
 	rawdata = thefile.read().strip().split('\n\n')
@@ -11,67 +21,129 @@ class ruleset:
 
 		self.rulesetraw = rulesetraw.split('\n')
 		self.numrules = len(self.rulesetraw)
-		self.revref = [set() for _ in range(self.numrules)] ##we will be able to look up all references to a particular index in ruleset. will init in readindata
-		self.letterindex = [] #useful to know where the letters are
-		
+		self.revref = {} ##we will be able to look up all references to a particular index in ruleset. will init in readindata
+		self.letteronlyrules = [] #add just the letters at first, then add any that are newly reduced to just letters
+		self.startletterindex = [] #helpful to know where our first 2 letters are
+		self.reducedregex = ""
+
 		#init ruleset
-		self.ruleset = []
+		self.ruleset = {}
 		self._readindata(self.rulesetraw)
 		
 
 	
 	def _readindata(self, rulesetraw):
 
-		refregp = r'(\d): ([\d ]+)(?:\|([\d ]+))?'
-		letregp = r'(\d): "([ab])"'
+		refregp = r'(\d+): ([\d ]+)(?:\|([\d ]+))?'
+		letregp = r'(\d+): "([ab])"'
 		for line in rulesetraw:
 			match = re.match(refregp, line)
 			if match:
 				#matched the first regex, so we're looking at a rule with only reference (not a letter rule)
 				matchnum = int(match.group(1))
 				matchset1 = [int(x) for x in match.group(2).strip().split(' ')]
-				for n in matchset1:
-					self.revref[n].add(matchnum) #add references to matchnum
+				#try matchset 2, if there is no | there will be no match
 				try:
-					matchset2 = [int(x) for x in match.group(3).strip().split(' ')]
-					for n in matchset2:
-						self.revref[n].add(matchnum) #add references to matchnum
+					matchset2 = [int(x) for x in match.group(3).strip().split(' ')]				
 				except:
-					matchset2 = None
-				self.ruleset.append([matchnum, matchset1, matchset2])
+					matchset2 = []
+				
+				for m in set(matchset1 + matchset2):
+					try:
+						#try adding to existing dict element, if fails, add a new one
+						self.revref[m] += [matchnum]
+					except KeyError:
+						self.revref[m] = [matchnum]
+
+				
+				self.ruleset[matchnum] = [matchset1, matchset2]
 			else:
 				#didn't match the firt regex, here are our letters
 				match = re.match(letregp, line)
 				try:
 					matchnum = int(match.group(1))
 					matchlet = match.group(2)
-					self.ruleset.append([matchnum, matchlet])
-					self.letterindex.append(matchnum)
+					self.ruleset[matchnum] = matchlet
+					self.startletterindex.append(matchnum)
+					self.letteronlyrules = self.startletterindex.copy()
 				except:
 					print("Failed to parse: " + line)
 	def ruletoregex(self, ruleindex):
 		##let's take a rule in the format: [3, ['a', 'b'], ['b', 'a']] and make it a regex string
+		##does not dereference, rule must only contain letters.
 		rule = self.ruleset[ruleindex]
-		try: 
-			if ruleindex not in self.letterindex:
-				if rule[2][0]:
-					string = '(' + ''.join(rule[1]) + '|' + ''.join(rule[2]) + ')'
-				else:
-					string = '(' + ''.join(rule[1]) + ')'
-			else: 
-				#letter rule, just return the letter
-				string = rule[1]
-		except: 
-			string = None
-			print("ruletoregex failed on ruleindex: " + str(ruleindex) + " : " + str(rule))
+		string = None
+		# try: 
+		if ruleindex not in self.startletterindex:
+			if rule[1]: 
+				string = '(' + ''.join(rule[0]) + '|' + ''.join(rule[1]) + ')'
+			else:
+				string = '(' + ''.join(rule[0]) + ')'
+		else: 
+			#letter rule, just return the letter
+			string = rule
+		# except: 
+		# 	string = None
+		# 	print("ruletoregex failed on ruleindex: " + str(ruleindex) + " : " + str(rule))
 		return string
+	def reduce(self, indextoreduce):
+		#for a given rule, get the regexification
+		tempstring = self.ruletoregex(indextoreduce)
+		#now find references to this one, and replace them with the string
+		if indextoreduce > 0:  
+			for ruleref in self.revref[indextoreduce]:
+				##for each rule that references this rule
+				for sideindex, sideofor in enumerate(self.ruleset[ruleref]):
+					if sideofor: #handling if there is no or in this rule
+						self.ruleset[ruleref][sideindex] = [tempstring if num == indextoreduce else num for num in sideofor]
+				try:##check if fully reduced
+					if type(1) not in [type(i) for i in (self.ruleset[ruleref][0] + self.ruleset[ruleref][1])]:
+						self.letteronlyrules.append(ruleref)
+				except TypeError: #if nothing after the or, will result in an error because nonetype
+					if type(1) not in [type(i) for i in self.ruleset[ruleref][0]]:
+						self.letteronlyrules.append(ruleref)
+		elif indextoreduce == 0: #only one that SHOULD have no references
+			self.ruleset[0][1] = tempstring
+
+	def reduceall(self):
+		while len(self.letteronlyrules) > 0:
+			self.reduce(self.letteronlyrules.pop(0))
+		self.reducedregex = self.ruleset[0][1]
+		self.reducedregex += r'\b'
+
+	def teststring(self, stringtotest):
+		#check if a passed in string is a valid rule
+		if self.letteronlyrules != "":
+			if re.match(self.reducedregex, stringtotest):
+				return True
+		else:
+			return False
 
 
 myruleset = ruleset(rawdata[0])
 
-for rule in myruleset.ruleset:
-	print(rule)
+print(myruleset.ruleset)
 
-print(myruleset.revref)
-print(myruleset.numrules)
-print(myruleset.ruletoregex(5))
+#print(myruleset.startletterindex)
+myruleset.reduceall()
+
+# myruleset.reduce(7)
+# myruleset.reduce(5)
+# myruleset.reduce(3)
+# myruleset.reduce(2)
+# myruleset.reduce(1)
+# myruleset.reduce(0)
+print(myruleset.ruleset)
+
+print(myruleset.reducedregex)
+
+messages = rawdata[1].split('\n')
+
+count = 0
+for m in messages:
+	print(m)
+	if myruleset.teststring(m):
+		print("is valid")
+		count += 1
+	
+print(count)
