1d592aac460f602b3290093cb62ca50a00f69525
Patrick Smith <pas3n7@gmail.com>
2020-12-29 23:47:00 -0600

    corrected num cards passed in, still not right

--- Day22/Day22.2.py
+++ Day22/Day22.2.py
@@ -1,5 +1,7 @@
-#fn = r'.\Day22\testinput.txt'
+# fn = r'.\Day22\testinput.txt'
 fn = r'.\Day22\input.txt'
+
+from copy import deepcopy
 
 
 with open(fn) as thefile:
@@ -7,16 +9,17 @@ with open(fn) as thefile:
 p1, p2 = map(lambda x : x.split('\n')[1:], (p1, p2))
 p1, p2 = map(lambda y : [int(x) for x in y], (p1, p2))
 
+# debugprint = print
+
+def debugprint(a, *args, **kwargs):
+	pass
 
 def statehash(p1deck, p2deck):
 	return int(''.join([str(x) for x in p1deck+p2deck]))
 
 def gamestate(p1card, p2card, p1deck, p2deck, gameround, gamedepth):
-	toprint = "\n-- Round {round} (Game {depth}) -- \nPlayer1 Deck: {p1d}\nPlayer2 Deck: {p2d}\nPlayer1 Plays: {p1}\nPlayer2 Plays: {p2}"
-	return toprint.format(round = gameround, depth = gamedepth, p1d = p1deck, p2d = p2deck, p1 = p1card, p2=p2card)
-
-
-	
+	toprint = "\n-- Round {round} (Game {depth}) -- \nPlayer1 Deck: {p1d}\nPlayer2 Deck: {p2d}\nPlayer1 Plays: {p1c}\nPlayer2 Plays: {p2c}"
+	return toprint.format(round = gameround, depth = gamedepth, p1d = p1deck, p2d = p2deck, p1c = p1card, p2c=p2card)
 
 
 def game(p1deck, p2deck, gamedepth=0):
@@ -26,18 +29,21 @@ def game(p1deck, p2deck, gamedepth=0):
 	while len(p1deck) > 0 and len(p2deck) > 0 and gameover == False:
 		p1wins = False
 		p2wins = False
-		startinghash = statehash(p1deck, p2deck)
+		startinghash = int(''.join([str(x) for x in p1deck+p2deck]))
 		if startinghash in gamehistory: #rule #1
 			p1wins = True
 			gameover = True
+			break
 		else:
-			p1card, p2card= p1deck.pop(0), p2deck.pop(0)
-		# print(gamestate(p1card, p2card, p1deck, p2deck, gameround, gamedepth))
-		#print("game: " + str(gamedepth) + "   round: " + str(gameround) + " size of history: " + str(len(gamehistory)), end="\r")
+			p1card, p2card= p1deck[0], p2deck[0]
+			p1deck = p1deck[1:]
+			p2deck = p2deck[1:]
+		debugprint(gamestate(p1card, p2card, p1deck, p2deck, gameround, gamedepth))
+		#debugprint("game: " + str(gamedepth) + "   round: " + str(gameround) + " size of history: " + str(len(gamehistory)), end="\r")
 		if not gameover:
 			if len(p1deck) >= p1card and len(p2deck) >= p2card:
-				#print("Playing a subgame to determine round winner")
-				subwinner, _ = game(p1deck[:p1card], p2deck[:p2card], gamedepth+1)  #don't need the deck of the winner
+				debugprint("Playing a subgame to determine round winner")
+				subwinner, _ = game(p1deck.copy()[:p1card+1], p2deck.copy()[:p2card+1], gamedepth+1)  #don't need the deck of the winner
 				if subwinner == 1:
 					p1wins = True
 				else:
@@ -49,25 +55,27 @@ def game(p1deck, p2deck, gamedepth=0):
 		
 			if p1wins:
 				p1deck.extend((p1card, p2card))
-				#print("Player1 Wins!")
+				debugprint("Player1 Wins!")
 			elif p2wins:
 				p2deck.extend((p2card, p1card))
-				#print("Player2 Wins!")
+				debugprint("Player2 Wins!")
 			else:
 				print("Oh no, nobody won")
 		gamehistory.append(startinghash)
 		gameround += 1
 
 
-	if gameover or p1deck>p2deck:
+	if gameover or len(p2deck)==0:
 		#only way we set gameover true is if something matched in the game history, p1 wins
 		winner = 1
 		winnerdeck = p1deck
-		#print("Player 1 wins game " + str(gamedepth))
-	else:
+		debugprint("Player 1 wins game " + str(gamedepth))
+	elif len(p1deck) == 0:
 		winner = 2
 		winnerdeck = p2deck
-		#print("Player 2 wins game " + str(gamedepth))
+		debugprint("Player 2 wins game " + str(gamedepth))
+	else:
+		print("oh no, nobody won the game")
 
 	return winner, winnerdeck
 
@@ -81,8 +89,9 @@ def calcscore(deck):
 
 
 #####################################Do things ####################################
-
-winner, winnerdeck = game(p1, p2)
+print(len(p1), len(p2))
+winner, winnerdeck = game(p1.copy(), p2.copy())
+print(len(p1), len(p2))
 
 print("player " + str(winner) + " wins!")
 print("score: " + str(calcscore(winnerdeck)))
